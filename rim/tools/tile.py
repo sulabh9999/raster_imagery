163f9153325b06431b2c59b180042405056f58ed
sulabh shukla <sulabh9999@gmail.com>
2020-03-27 10:26:51 +0530

    adding exceptions

--- rim/tools/tile.py
+++ rim/tools/tile.py
@@ -1,6 +1,6 @@
 import os
 
-# create a Polygon box to chip into training image and label 
+# create a Polygon box to chip into training image and label
 from shapely.geometry import box
 from pyproj import CRS
 import geopandas as gpd
@@ -9,129 +9,111 @@ from rasterio.features import rasterize
 
 def add_parser(subparser):
 
-	# print('args in subparser: ', args)
-	parser = subparser.add_parser('tile_paser', help='parser for tile')
+    # print('args in subparser: ', args)
+    parser = subparser.add_parser('tile_paser', help='parser for tile')
 
-	inp = parser.add_argument_group("Inputs")
-	inp.add_argument("--tiff", type=str, required=True, help="tif files to rasterize [required]")
-	inp.add_argument("--geojson", type=str, required=True, help="source geojson for ploting homes [required]")
-	inp.add_argument("--size", type=str, default="512,512", help="size of tile (h, w) [required]")
-	
-	out = parser.add_argument_group("Output")
-	out.add_argument("--out", type=str, required=True, help="path to store tiles")
+    inp = parser.add_argument_group("Inputs")
+    inp.add_argument("--tif",
+                     type=str,
+                     required=True,
+                     help="tif files to rasterize [required]")
+    inp.add_argument("--geojson",
+                     type=str,
+                     required=True,
+                     help="source geojson for ploting homes [required]")
+    inp.add_argument("--size",
+                     type=str,
+                     default="512,512",
+                     help="size of tile h,w default 512,512")
 
-	args, unknown = parser.parse_known_args()
+    out = parser.add_argument_group("Output")
+    out.add_argument("--out",
+                     type=str,
+                     required=True,
+                     help="path to store tiles")
 
-	parser.set_defaults(func=main)
-	print('args in main: ', args)
+    parser.set_defaults(func=main)
+    args, unknown = parser.parse_known_args()
 
-	# args.func(args)
-
-
-
-
-
-
-	# ================
-	# parser = subparser.add_parser("tile", help="Helps to create tiles")
-
-	# inp = parser.add_argument_group("Inputs")
-	# inp.add_argument("--tiff", type=str, required=True, default=None, nargs='+', help="tif files to rasterize [required]")
-	# # inp.add_argument("--geojson", type=str, required=True, help="source geojson for ploting homes [required]")
-	# # inp.add_argument("--size", type=str, default="512,512", help="size of tile (h, w) [required]")
-	
-	# # out = parser.add_argument_group("Output")
-	# # out.add_argument("--out", type=str, required=True, help="path to store tiles")
-
-	# # print('parser: ', parser)
-	# parser.set_defaults(func=main)
-	# # main(parser)
-	# args = parser.parse_args()
-	# args.func(args)
+    args.func(args)
 
 
-def rasterize(tif, geojson, window_size, stride=None):
-	img_arr = None
-	img_window = None
+def rasterize(args, stride=None):
+    """
+	start rasterizing tif file. here we have window with (args.size) and chunking tif image
+	with size. this image are numpy array which is not stored in disk at this time. Geojson
+	will be applyed on these image chunks with proper transformation for each polygone.
 
-	scene_labels_gdf = gpd.read_file(geojson)	
+	chunks and masked images will be used as training dataset for Deep Learning model.
+	tif image can be extra large, tools is able to handle it.
+	"""
+    scene_labels_gdf = gpd.read_file(args.geojson)
 
-	img_list = [i for i in get_image_chunks(rst, window_size=window_size, img_count=100)]
+    img_list = [i for i in get_image_chunks(args.tif, window_size=args.size)]
 
-	for index, each in enumerate(img_list[:20]):
-		# index = 8
-		# for win, arr in get_image_chunks(rst, window_size=(win_sz, win_sz)):
-		img_window, img_arr = each[0], each[1] #img_list[index][0], img_list[index][1]
+    for index, each in enumerate(img_list[:20]):
+        # for win, arr in get_image_chunks(rst, window_size=(win_sz, win_sz)):
+        img_window, img_arr = each[0], each[1]
 
-		win_arr = np.moveaxis(img_arr,0,2)
-		# plt.figure(figsize=(5,5))
-		# plt.imshow(win_arr)
+        # 'miny', 'maxx', and 'maxy'
+        # (807592.8560103609, 620885.095643373, 807611.1959577247, 620903.4357975163)
+        bounds = rasterio.windows.bounds(img_window, rst.meta['transform'])
 
+        # shapely.geometry.polygon.Polygon for full image chunk (512, 512) in tiff
+        win_box = box(*bounds)
+        win_box_gdf = gpd.GeoDataFrame(geometry=[win_box], crs=rst.meta['crs'])
+        win_box_gdf = win_box_gdf.to_crs(CRS.from_epsg(4326))
 
+        gdf_chip = gpd.sjoin(scene_labels_gdf,
+                             win_box_gdf,
+                             how='inner',
+                             op='intersects')
 
-		# 'miny', 'maxx', and 'maxy'
-		# (807592.8560103609, 620885.095643373, 807611.1959577247, 620903.4357975163)
-		bounds = rasterio.windows.bounds(img_window, rst.meta['transform'])
+        if not gdf_chip.empty:
+            burn_val = 255
+            shapes = [(geom, burn_val) for geom in gdf_chip.geometry]
 
-		# shapely.geometry.polygon.Polygon for full image chunk (512, 512) in tiff
-		win_box = box(*bounds)
-		win_box_gdf = gpd.GeoDataFrame(geometry=[win_box], crs=rst.meta['crs'])
-		win_box_gdf = win_box_gdf.to_crs(CRS.from_epsg(4326))
+            chip_tfm = rasterio.transform.from_bounds(
+                *win_box_gdf.bounds.values[0], win_sz, win_sz)
+            label_arr = rasterize(shapes, (win_sz, win_sz), transform=chip_tfm)
 
+            fig = plt.figure()
 
-		gdf_chip = gpd.sjoin(scene_labels_gdf, win_box_gdf, how='inner', op='intersects')
-		# print(gdf_chip)
+            win_arr = np.moveaxis(img_arr, 0, 2)
+            ax1 = fig.add_subplot(1, 2, 1)
+            ax1.imshow(win_arr)
 
-		if not gdf_chip.empty:
-			burn_val = 255
-			shapes = [(geom, burn_val) for geom in gdf_chip.geometry]
-			shapes[0][0]
+            ax2 = fig.add_subplot(1, 2, 2)
+            ax2.imshow(win_arr)
 
-			chip_tfm = rasterio.transform.from_bounds(*win_box_gdf.bounds.values[0], win_sz, win_sz)
-			label_arr = rasterize(shapes, (win_sz, win_sz), transform=chip_tfm)
+            ax1 = fig.add_subplot(1, 2, 2)
+            ax1.imshow(label_arr, alpha=0.5)
 
-			fig = plt.figure()
-
-			ax1 = fig.add_subplot(1,2,1)
-			ax1.imshow(win_arr)
-
-			ax2 = fig.add_subplot(1,2,2)
-			ax2.imshow(win_arr)
-
-			ax1 = fig.add_subplot(1,2,2)
-			ax1.imshow(label_arr, alpha=0.5)
-
-			print(label_arr.shape)
-			break
-
-
-
+            print(label_arr)
+            break
 
 
 def main(args):
+    """
+	Validation for arguments and invoke function
+	"""
+    # --tif
+    if args.tif and not os.path.isfile(args.tif):
+        raise ValueError(f"incorrect tif file: {args.tif}")
 
-	print('i am in main in tile', args)
+    # --geojson
+    if args.geojson and not os.path.isfile(args.geojson):
+        raise ValueError(f"incorrect geojson file: {args.geojson}")
 
-	# if args.tif and not os.path.exist(args.tif):
-	# 	raise ValueError("Doesn't exist --tiff file")
+    # --size
+    try:
+        args.size = tuple(map(int, args.size.split(",")))
+    except:
+        raise ValueError("invalid --size value, use --help")
 
-    # try:
-    #     args.size = list(map(int, args.bands.split(","))) if args.bands else None
-    # except:
-    #     raise ValueError("invalid --args.bands value")
+    # --out
+    if args.out and not os.path.exists(args.out):
+        os.makedirs(args.out)
 
-    # try:
-    #     args.bands = list(map(int, args.bands.split(","))) if args.bands else None
-    # except:
-    #     raise ValueError("invalid --args.bands value")
-
-    # try:
-    #     args.bands = list(map(int, args.bands.split(","))) if args.bands else None
-    # except:
-    #     raise ValueError("invalid --args.bands value")
-
-
-	# rasterize(args.tif, arg.geojson, args.window_size)
-	
-
-	
+    # start rasterizing
+    rasterize(args)
--- rim/utils/tiffUtils.py
+++ rim/utils/tiffUtils.py
@@ -1,17 +1,27 @@
-
-def save_geochip_as_tif(arr, chip_tfm, name='test', crs='EPSG:4326', dtype='uint8'):
+def save_geochip_as_tif(arr,
+                        chip_tfm,
+                        name='test',
+                        crs='EPSG:4326',
+                        dtype='uint8'):
     im = (arr).astype(dtype)
 
     # check im shape, number of channels and expand into (H,W,C) if needed
-    if len(im.shape) == 3: 
-      num_ch = im.shape[-1]
-    else: 
-      num_ch = 1
-      im = np.expand_dims(im, -1)
+    if len(im.shape) == 3:
+        num_ch = im.shape[-1]
+    else:
+        num_ch = 1
+        im = np.expand_dims(im, -1)
 
-    with rasterio.open(f'{name}.tif', 'w', driver='GTiff', 
-                            height=im.shape[0], width=im.shape[1],
-                            count=num_ch, dtype=im.dtype, crs=crs, transform=chip_tfm, compress='LZW') as dst: 
-        
+    with rasterio.open(f'{name}.tif',
+                       'w',
+                       driver='GTiff',
+                       height=im.shape[0],
+                       width=im.shape[1],
+                       count=num_ch,
+                       dtype=im.dtype,
+                       crs=crs,
+                       transform=chip_tfm,
+                       compress='LZW') as dst:
+
         for ch in range(num_ch):
-          dst.write(im[:,:,ch], indexes=ch+1)
+            dst.write(im[:, :, ch], indexes=ch + 1)
